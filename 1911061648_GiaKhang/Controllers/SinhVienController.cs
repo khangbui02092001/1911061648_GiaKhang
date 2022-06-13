591a02cb5cf1696ad5989b940becd57861d5a58d
Acer <Acer@DESKTOP-2DES107>
2022-06-13 15:25:51 +0700

    Add project files.

---
+++ 1911061648_GiaKhang/Controllers/SinhVienController.cs
@@ -0,0 +1,122 @@
+﻿using _1911061648_GiaKhang.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _1911061648_GiaKhang.Controllers
+{
+    public class SinhVienController : Controller
+    {
+        // GET: SinhVien
+        public ActionResult Index()
+        {
+            return View();
+        }
+        Test01DataContext data = new Test01DataContext();
+        public ActionResult ListSinhVien()
+        {
+            var all_sinhvien = from ss in data.SinhViens select ss;
+            return View(all_sinhvien);
+        }
+        public ActionResult Detail(string id)
+        {
+            var D_sinhvien = data.SinhViens.Where(m => m.MaSV == id).First();
+            return View(D_sinhvien);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(FormCollection collection, SinhVien s)
+        {
+            var E_masv = collection["MaSV"];
+            var E_hoten = collection["HoTen"];
+            var E_gioitinh = collection["GioiTinh"];
+            var E_ngaysinh = Convert.ToDateTime(collection["NgaySinh"]);
+            var E_hinh = collection["Hinh"];
+            var E_manganh = collection["MaNganh"];
+            if (string.IsNullOrEmpty(E_masv))
+            {
+                ViewData["Error"] = "Don't empty!";
+            }
+            else
+            {
+                s.MaSV = E_masv.ToString();
+                s.HoTen = E_hoten;
+                s.GioiTinh = E_gioitinh;
+                s.NgaySinh = E_ngaysinh;
+                s.Hinh = E_hinh.ToString();
+                s.MaNganh = E_manganh;
+                
+                data.SinhViens.InsertOnSubmit(s);
+                data.SubmitChanges();
+                return RedirectToAction("ListSinhVien");
+            }
+            return this.Create();
+        }
+
+        public ActionResult Edit(string id)
+        {
+            var E_sinhvien = data.SinhViens.First(m => m.MaSV == id);
+            return View(E_sinhvien);
+        }
+        [HttpPost]
+        public ActionResult Edit(string id, FormCollection collection)
+        {
+            var E_masv = data.SinhViens.First(m => m.MaSV == id);
+            var E_hoten = collection["HoTen"];
+            var E_gioitinh = collection["GioiTinh"];
+            var E_ngaysinh = Convert.ToDateTime(collection["NgaySinh"]);
+            var E_hinh = collection["Hinh"];
+            var E_manganh = collection["MaNganh"];
+
+
+            E_masv.MaSV = id;
+
+            if (string.IsNullOrEmpty(E_hoten))
+            {
+                ViewData["Error"] = "Don't empty!";
+            }
+            else
+            {
+                E_masv.MaSV = E_masv.ToString();
+                E_masv.HoTen = E_hoten;
+                E_masv.GioiTinh = E_gioitinh;
+                E_masv.NgaySinh = E_ngaysinh;
+                E_masv.Hinh = E_hinh.ToString();
+                E_masv.MaNganh = E_manganh;
+                UpdateModel(E_masv);
+                data.SubmitChanges();
+                return RedirectToAction("ListSinhVien");
+            }
+            return this.Edit(id);
+        }
+        public string ProcessUpload(HttpPostedFileBase file)
+        {
+            if (file == null)
+            {
+                return "";
+            }
+            file.SaveAs(Server.MapPath("~/Content/images/" + file.FileName));
+            return "/Content/images/" + file.FileName;
+        }
+
+        public ActionResult Delete(string id)
+        {
+            var D_sinhvien = data.SinhViens.First(m => m.MaSV == id);
+            return View(D_sinhvien);
+        }
+        [HttpPost]
+        public ActionResult Delete(string id, FormCollection collection)
+        {
+            var D_sinhvien = data.SinhViens.Where(m => m.MaSV == id).First();
+            data.SinhViens.DeleteOnSubmit(D_sinhvien);
+            data.SubmitChanges();
+            return RedirectToAction("ListSinhVien");
+        }
+    }
+}
---
+++ 1911061648_GiaKhang/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_1911061648_GiaKhang.MvcApplication" Language="C#" %>
